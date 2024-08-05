baf14599809bc97e13774dea119eb354cf118e25
ndourmouhammad <ndourmouhammad@gmail.com>
2024-08-05 11:33:57 +0000

    Mise en place des seeders

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -2,8 +2,10 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\User;
+use Illuminate\Support\Str;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class UserSeeder extends Seeder
 {
@@ -12,6 +14,66 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $users = [
+            [
+                'nom' => "Biteye",
+                'prenom' => "Ndeye",
+                'email' => "ndeye@gmail.com",
+                "role" => "admin",
+                'email_verified_at' => now(),
+                'password' => Hash::make('password'),
+                'remember_token' => Str::random(10),
+            ],
+
+            [
+                'nom' => "Talla",
+                'prenom' => "Cheikh Saliou",
+                'email' => "cheikhSaliou@gmail.com",
+                "role" => "personnel",
+                'email_verified_at' => now(),
+                'password' => Hash::make('password'),
+                'remember_token' => Str::random(10),
+            ],
+            [
+                'nom' => "Kane",
+                'prenom' => "Marieme",
+                'email' => "marieme@gmail.com",
+                "role" => "personnel",
+                'email_verified_at' => now(),
+                'password' => Hash::make('password'),
+                'remember_token' => Str::random(10),
+            ],
+            [
+                'nom' => "Ba",
+                'prenom' => "Magid",
+                'email' => "magid@gmail.com",
+                "role" => "membre",
+                'email_verified_at' => now(),
+                'password' => Hash::make('password'),
+                'remember_token' => Str::random(10),
+            ],
+            [
+                'nom' => "Diallo",
+                'prenom' => "Alpha",
+                'email' => "alpha@gmail.com",
+                "role" => "membre",
+                'email_verified_at' => now(),
+                'password' => Hash::make('password'),
+                'remember_token' => Str::random(10),
+            ],
+            [
+                'nom' => "Diaw",
+                'prenom' => "Yaye Rokhaya",
+                'email' => "yaye@gmail.com",
+                "role" => "membre",
+                'email_verified_at' => now(),
+                'password' => Hash::make('password'),
+                'remember_token' => Str::random(10),
+            ],
+        ];
+        foreach ($users as $user) {
+            User::create($user);
+        }
     }
 }
+?>
